e05c51a5b820ea011b6182a41cfdd458cf4677cf
Sebastian Petak <sebastianpetak@gmail.com>
2017-01-09 13:18:58 -0600

    42%

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,6 +7,7 @@ var IndexRoute = ReactRouter.IndexRoute;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer');
 
 
 // Main component is always active
@@ -16,6 +17,10 @@ var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
+      <Route path='playerOne' header='Player One'
+        component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header ='Player Two'
+        component={PromptContainer} />
     </Route>
   </Router>
 );
